b0bd6488d7d41f0f13fc523604cddbe524d300b9
sunagaitsuki <i.sunaga.touroku@gmail.com>
2024-06-09 09:18:39 +0900

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,9 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        他のにゃんこも見る
+        One More Cat!
       </button>
-      <div className={styles.frame}>
-        {loading || <img src={imageUrl} className={styles.img} />}
-      </div>
+      <div className={styles.frame}>{loading || <img src={imageUrl} />}</div>
     </div>
   );
 };
